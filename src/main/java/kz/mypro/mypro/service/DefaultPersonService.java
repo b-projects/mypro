a52d186f7bc13a7deaa988f40970b083bf406666
Bake <batyrkhan.orazbayev@gmail.com>
2023-03-02 21:30:47 +0600

    xml

--- src/main/java/kz/mypro/mypro/service/DefaultPersonService.java
+++ src/main/java/kz/mypro/mypro/service/DefaultPersonService.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.xml.bind.JAXB;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.StringWriter;
 import java.util.Date;
 
@@ -18,7 +20,10 @@ public class DefaultPersonService implements PersonService{
 
     @Override
     public void searchPersonByIin(String iin){
+
+        String filename = "request.xml";
         QueryMsg queryMsg = new QueryMsg();
+
 
         FindPersonRequest findPersonRequest = new FindPersonRequest();
         findPersonRequest.setIin(iin);
@@ -30,6 +35,15 @@ public class DefaultPersonService implements PersonService{
         queryMsg.setMsg_type(1); //  Тип запроса: 1 - запрос, 2-ответ
         queryMsg.setMsg_status(1); // Статус запроса: 1-На отправку
         queryMsg.setMsg_text(swrequest.toString());
+
+        try {
+            FileOutputStream fileOutputStream =new FileOutputStream(new File(filename));
+            byte[] buffer = swrequest.toString().getBytes();
+            fileOutputStream.write(buffer, 0, buffer.length);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         personRepository.save(queryMsg);
     }
